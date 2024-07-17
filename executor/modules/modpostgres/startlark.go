e5ba3a7da0a2f78dab68bae57538b9ab1ab1e3a4
protosam <samigknighted@gmail.com>
2024-07-17 18:54:05 -0500

    rename moddb to modpostgres

---
+++ executor/modules/modpostgres/startlark.go
@@ -0,0 +1,34 @@
+package modpostgres
+
+import (
+	"log"
+
+	"go.starlark.net/starlark"
+)
+
+func pgargsToStarlarkValue(_ *starlark.Thread, fn *starlark.Builtin, pgargs *starlark.List) []interface{} {
+	params := []interface{}{}
+	for i := 0; i < pgargs.Len(); i++ {
+		param := pgargs.Index(i)
+		switch param.Type() {
+		case "bool":
+			params = append(params, param.Truth())
+		case "int":
+			var paramToGoVal int
+			starlark.AsInt(param, &paramToGoVal)
+			params = append(params, paramToGoVal)
+		case "float":
+			paramToGoVal, _ := starlark.AsFloat(param)
+			params = append(params, paramToGoVal)
+		case "string":
+			paramToGoVal, _ := starlark.AsString(param)
+			params = append(params, paramToGoVal)
+		default:
+			paramToGoVal, _ := starlark.AsString(param)
+			params = append(params, paramToGoVal)
+			log.Printf("failed back to Sprintf for type in %s(): %#v", fn.Name(), param)
+		}
+	}
+
+	return params
+}
